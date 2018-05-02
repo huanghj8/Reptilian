5968c1804a5ae18f0587534c20e03fd70d415404
huanghj8 <huanghj8@163.com>
2018-05-02 21:51:37 +0800

    Annotate the task5 and task6. Bugs need to be fixed later.

--- DoubanCrawler.py
+++ DoubanCrawler.py
@@ -76,44 +76,44 @@ writer.writerow(my_list3)
 f.close()
 
 
-# 任務6：將每個電影類別按地區電影數量排序，前三名的地區是哪些，佔比多少，結果輸出到文件output.txt
-def putMax(mylist, location = ""):
-    input_dict = {'中国大陆': 0, '美国': 0, '香港': 0, '台湾': 0, '日本': 0, '韩国': 0, '英国': 0, '法国': 0, '德国': 0, '意大利': 0, '西班牙': 0,
-                  '印度': 0, '泰国': 0, '俄罗斯': 0, '伊朗': 0, '加拿大': 0, '澳大利亚': 0, '爱尔兰': 0, '瑞典': 0, '巴西': 0, '丹麦': 0}
-    total = 0
-    for movie in mylist:
-        if location in movie:
-            input_dict[location] += 1
-            total += 1
-    for idict in input_dict:
-        while total > 0:
-            input_dict[idict] = round((input_dict[idict] / total) * 100, 2)
-    return sorted(input_dict.items(), key=lambda x: x[1], reverse=True)[:3]
-
-
-def turnTostr(max_tuple):
-    result_list = []
-    i = 0
-    while i < len(max_tuple):
-        element = max_tuple[i]
-        ele = str(element)
-        e = "{}占百分之{},排名第{}".format(ele[2:ele.index(',') - 1], ele[ele.index(',') + 2: -3], i + 1)
-        i += 1
-        result_list.append(e)
-    return result_list
-
-
-max_list = [turnTostr(putMax(my_list1)), turnTostr(putMax(my_list2)), turnTostr(putMax(my_list3))]
-
-f = open('output.txt', 'w')
-f.write("我選擇電影類型為科幻、歌舞和歷史。\n")
-j = 0
-while j < len(category_list):
-    f.write("{}類型電影數量排名前三的地區和百分比為：\n".format(category_list[j]))
-    i = 0
-    while i < len(max_list[j]):
-        f.write(max_list[j][i])
-        f.write('\n')
-        i += 1
-    j += 1
-f.close()
+# # 任務6：將每個電影類別按地區電影數量排序，前三名的地區是哪些，佔比多少，結果輸出到文件output.txt
+# def putMax(mylist, location = ""):
+#     input_dict = {'中国大陆': 0, '美国': 0, '香港': 0, '台湾': 0, '日本': 0, '韩国': 0, '英国': 0, '法国': 0, '德国': 0, '意大利': 0, '西班牙': 0,
+#                   '印度': 0, '泰国': 0, '俄罗斯': 0, '伊朗': 0, '加拿大': 0, '澳大利亚': 0, '爱尔兰': 0, '瑞典': 0, '巴西': 0, '丹麦': 0}
+#     total = 0
+#     for movie in mylist:
+#         if location in movie:
+#             input_dict[location] += 1
+#             total += 1
+#     for idict in input_dict:
+#         while total > 0:
+#             input_dict[idict] = round((input_dict[idict] / total) * 100, 2)
+#     return sorted(input_dict.items(), key=lambda x: x[1], reverse=True)[:3]
+#
+#
+# def turnTostr(max_tuple):
+#     result_list = []
+#     i = 0
+#     while i < len(max_tuple):
+#         element = max_tuple[i]
+#         ele = str(element)
+#         e = "{}占百分之{},排名第{}".format(ele[2:ele.index(',') - 1], ele[ele.index(',') + 2: -3], i + 1)
+#         i += 1
+#         result_list.append(e)
+#     return result_list
+#
+#
+# max_list = [turnTostr(putMax(my_list1)), turnTostr(putMax(my_list2)), turnTostr(putMax(my_list3))]
+#
+# f = open('output.txt', 'w')
+# f.write("我選擇電影類型為科幻、歌舞和歷史。\n")
+# j = 0
+# while j < len(category_list):
+#     f.write("{}類型電影數量排名前三的地區和百分比為：\n".format(category_list[j]))
+#     i = 0
+#     while i < len(max_list[j]):
+#         f.write(max_list[j][i])
+#         f.write('\n')
+#         i += 1
+#     j += 1
+# f.close()
